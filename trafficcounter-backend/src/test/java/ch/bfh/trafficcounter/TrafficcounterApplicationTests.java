892530821780999576792428bf645f940f449459
Manuel Riesen <manuel.riesen0@gmail.com>
2022-10-07 09:54:27 +0200

    create basic backend structure

---
+++ trafficcounter-backend/src/test/java/ch/bfh/trafficcounter/TrafficcounterApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bfh.trafficcounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficcounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
