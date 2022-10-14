fb90876aa343236d8df256dea32e01cca3773394
Sven Trachsel <sven.projects@protonmail.ch>
2022-10-14 16:05:22 +0200

    no init in root Application

--- trafficcounter-backend/src/main/java/ch/bfh/trafficcounter/TrafficcounterApplication.java
+++ trafficcounter-backend/src/main/java/ch/bfh/trafficcounter/TrafficcounterApplication.java
@@ -1,13 +1,32 @@
 package ch.bfh.trafficcounter;
 
+import ch.bfh.trafficcounter.service.api.OpenTransportDataApiService;
+import ch.bfh.trafficcounter.service.api.OpenTransportDataApiServiceImpl;
+import ch.opentdata.wsdl.D2LogicalModel;
+import ch.opentdata.wsdl.PullService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class TrafficcounterApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(TrafficcounterApplication.class, args);
 	}
+
+	@Autowired
+	private OpenTransportDataApiService api;
+
+	/*
+	@PostConstruct
+	public void init() {
+		D2LogicalModel response = api.pullMeasuredData();
+		System.out.println();
+	}
+	 */
 
 }
