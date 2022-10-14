a2a57e1096dab2c37d6947e7a29fed505d36ab07
Sven Trachsel <sven.projects@protonmail.ch>
2022-10-14 15:44:54 +0200

    finished endpoint for static data

--- trafficcounter-backend/src/main/java/ch/bfh/trafficcounter/controller/MeasurementPointController.java
+++ trafficcounter-backend/src/main/java/ch/bfh/trafficcounter/controller/MeasurementPointController.java
@@ -1,11 +1,30 @@
 package ch.bfh.trafficcounter.controller;
 
+import ch.bfh.trafficcounter.model.dto.geojson.GeoJsonFeatureCollectionDto;
+import ch.bfh.trafficcounter.service.MeasurementPointService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 
 @RestController
-@RequestMapping("/api/modules")
+@RequestMapping("/api/measurementpoints")
 public class MeasurementPointController {
 
+	private final MeasurementPointService measurementPointService;
+
+	public MeasurementPointController(MeasurementPointService measurementPointService) {
+		this.measurementPointService = measurementPointService;
+	}
+
+	@GetMapping
+	public ResponseEntity<GeoJsonFeatureCollectionDto> getMeasurementPointsGeoJson() {
+		return ResponseEntity.ok(measurementPointService.getAllMeasurementPointsGeoJson());
+	}
+
+
 }
+
