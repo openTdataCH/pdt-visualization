d59de56ad1507034f834ae7ec5276559aad1b3e8
Sven Trachsel <sven.projects@protonmail.ch>
2022-10-15 13:22:04 +0200

    added repository for MeasurementPoint

--- trafficcounter-backend/src/main/java/ch/bfh/trafficcounter/repository/MeasurementPointRepository.java
+++ trafficcounter-backend/src/main/java/ch/bfh/trafficcounter/repository/MeasurementPointRepository.java
@@ -1,10 +1,11 @@
 package ch.bfh.trafficcounter.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface MeasurementPointRepository extends JpaRepository<Module, UUID>{
+public interface MeasurementPointRepository extends JpaRepository<Module, UUID> {
 
 }
