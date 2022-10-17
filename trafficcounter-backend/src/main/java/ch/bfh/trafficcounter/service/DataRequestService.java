7c0dfac57f47f0d9fae340d24c5f9ea0c7954833
Sven Trachsel <sven.projects@protonmail.ch>
2022-10-17 18:02:40 +0200

    request and persist data implementation

--- trafficcounter-backend/src/main/java/ch/bfh/trafficcounter/service/DataRequestService.java
+++ trafficcounter-backend/src/main/java/ch/bfh/trafficcounter/service/DataRequestService.java
@@ -1,44 +1,87 @@
 package ch.bfh.trafficcounter.service;
 
+import ch.bfh.trafficcounter.model.entity.MeasurementPoint;
 import ch.bfh.trafficcounter.repository.MeasurementPointRepository;
 import ch.bfh.trafficcounter.service.api.OpenTransportDataApiService;
+import ch.opentdata.wsdl.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * Manages querying the api and saving the data to the database
  */
+@Service
+@EnableScheduling
 public class DataRequestService {
 
-	private OpenTransportDataApiService api;
-	private final MeasurementPointRepository mpRepo;
+	private final OpenTransportDataApiService api;
+	private final MeasurementPointRepository mPRepo;
+
+	private boolean firstStart = true;
+
 	@Autowired
 	public DataRequestService(
 			OpenTransportDataApiService api,
-			MeasurementPointRepository mpRepo
+			MeasurementPointRepository mPRepo
 	) {
 		this.api = api;
-		this.mpRepo = mpRepo;
+		this.mPRepo = mPRepo;
 	}
 
-	@PostConstruct
-	public void init() {
-		//TODO, request data and save into database
+	@Scheduled(fixedRate = 3600000) // do it every hour, starting immediately
+	public void requestAndPersistData() {
+		if (firstStart) {
+			firstStart = false;
+			requestAndPersistStaticData();
+		}
+		requestAndPersistDynamicData();
+	}
 
+	private void requestAndPersistStaticData() {
+		ArrayList<MeasurementPoint> measurementPoints = new ArrayList<>();
+		D2LogicalModel staticData = api.pullMeasurementSiteTable();
+		PayloadPublication payloadPublication = staticData.getPayloadPublication();
+		if (!(payloadPublication instanceof final MeasurementSiteTablePublication mSTP)) {
+			throw new ClassCastException("Expected MeasurementSiteTablePublication, but was not");
+		}
 
-		//ArrayList<MeasurementPoint> measurementPoints;
-		//D2LogicalModel staticData = api.pullMeasurementSiteTable();
+		for (MeasurementSiteRecord mSRecord : mSTP.getMeasurementSiteTable().get(0).getMeasurementSiteRecord()) {
 
-		//staticData.getPayloadPublication();
+			if (!(mSRecord.getMeasurementSiteLocation() instanceof final Point pt)) {
+				throw new ClassCastException("Expected Point, but was not");
+			}
 
-		//measurementPointRepository.saveAll(measurementPoints);
+			// extract coordinates
+			PointByCoordinates ptByCoords = pt.getPointByCoordinates();
+			if (ptByCoords == null) {
+				System.out.println(String.format("Skipping site %s, no coordinates", mSRecord.getId()));
+				continue;
+			}
 
+			double[] coordinates = {ptByCoords.getPointCoordinates().getLatitude(), ptByCoords.getPointCoordinates().getLongitude()};
 
-		//D2LogicalModel response = api.pullMeasurementSiteTable();
-		System.out.println();
-		//Meas
+			measurementPoints.add(
+				MeasurementPoint.builder()
+					.id(mSRecord.getId())
+					.latitude(coordinates[0])
+					.longtitude(coordinates[1])
+					.numberOfLanes(mSRecord.getMeasurementSiteNumberOfLanes().intValue())
+					.active(true)
+					.build()
+			);
+		}
+		mPRepo.saveAll(measurementPoints);
+	}
 
+	private void requestAndPersistDynamicData() {
+		//TODO, use once dynamic data is used
 	}
 
 }
