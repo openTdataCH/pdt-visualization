4b15a8446e5b551e5e0ae04a3b5a150a55c6f4f0
Sven Trachsel <sven.projects@protonmail.ch>
2022-10-17 09:56:29 +0200

    added getters and setters

--- trafficcounter-backend/src/main/java/ch/bfh/trafficcounter/model/entity/MeasurementPoint.java
+++ trafficcounter-backend/src/main/java/ch/bfh/trafficcounter/model/entity/MeasurementPoint.java
@@ -14,4 +14,36 @@ public class MeasurementPoint {
 	private double longtitude;
 
 	private int numberOfLanes;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLongtitude() {
+		return longtitude;
+	}
+
+	public void setLongtitude(double longtitude) {
+		this.longtitude = longtitude;
+	}
+
+	public int getNumberOfLanes() {
+		return numberOfLanes;
+	}
+
+	public void setNumberOfLanes(int numberOfLanes) {
+		this.numberOfLanes = numberOfLanes;
+	}
 }
