59ff93ec4e7d8567c2187315c12c3ad81a31995e
Sven Trachsel <sven.projects@protonmail.ch>
2022-10-17 09:58:16 +0200

    implemented dtomapper to create geoJson

---
+++ trafficcounter-backend/src/main/java/ch/bfh/trafficcounter/mapper/DtoMapper.java
@@ -0,0 +1,19 @@
+package ch.bfh.trafficcounter.mapper;
+
+import ch.bfh.trafficcounter.model.dto.geojson.GeoJsonFeatureCollectionDto;
+import ch.bfh.trafficcounter.model.dto.geojson.GeoJsonFeatureDto;
+import ch.bfh.trafficcounter.model.dto.geojson.GeoJsonGeometryDto;
+import ch.bfh.trafficcounter.model.dto.geojson.GeoJsonPropertiesDto;
+import ch.bfh.trafficcounter.model.entity.MeasurementPoint;
+
+import java.util.ArrayList;
+
+public interface DtoMapper {
+	GeoJsonFeatureCollectionDto mapMeasurementPointsToGeoJsonFeatureCollectionDto(ArrayList<MeasurementPoint> measurementPoint);
+
+	GeoJsonFeatureDto mapMeasurementPointToGeoJsonFeatureDto(MeasurementPoint measurementPoint);
+
+	GeoJsonGeometryDto mapMeasurementPointToGeoJsonGeometryDto(MeasurementPoint measurementPoint);
+
+	GeoJsonPropertiesDto mapMeasurementPointToGeoJsonPropertiesDto(MeasurementPoint measurementPoint);
+}
---
+++ trafficcounter-backend/src/main/java/ch/bfh/trafficcounter/mapper/DtoMapperImpl.java
@@ -0,0 +1,44 @@
+package ch.bfh.trafficcounter.mapper;
+
+import ch.bfh.trafficcounter.model.dto.geojson.GeoJsonFeatureCollectionDto;
+import ch.bfh.trafficcounter.model.dto.geojson.GeoJsonFeatureDto;
+import ch.bfh.trafficcounter.model.dto.geojson.GeoJsonGeometryDto;
+import ch.bfh.trafficcounter.model.dto.geojson.GeoJsonPropertiesDto;
+import ch.bfh.trafficcounter.model.entity.MeasurementPoint;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+@Component
+public class DtoMapperImpl implements DtoMapper {
+
+	@Override
+	public GeoJsonFeatureCollectionDto mapMeasurementPointsToGeoJsonFeatureCollectionDto(ArrayList<MeasurementPoint> measurementPoints) {
+		// check for empty array
+		if (measurementPoints == null || measurementPoints.size() == 0) {
+			return null;
+		}
+
+		ArrayList<GeoJsonFeatureDto> featureDtos = new ArrayList<>();
+		for (MeasurementPoint mp : measurementPoints) {
+			featureDtos.add(mapMeasurementPointToGeoJsonFeatureDto(mp));
+		}
+
+		return new GeoJsonFeatureCollectionDto(featureDtos);
+	}
+
+	@Override
+	public GeoJsonFeatureDto mapMeasurementPointToGeoJsonFeatureDto(MeasurementPoint mp) {
+		return new GeoJsonFeatureDto(mapMeasurementPointToGeoJsonGeometryDto(mp), mapMeasurementPointToGeoJsonPropertiesDto(mp));
+	}
+
+	@Override
+	public GeoJsonGeometryDto mapMeasurementPointToGeoJsonGeometryDto(MeasurementPoint mp) {
+		return new GeoJsonGeometryDto(new Double[]{mp.getLongtitude(), mp.getLatitude()});
+	}
+
+	@Override
+	public GeoJsonPropertiesDto mapMeasurementPointToGeoJsonPropertiesDto(MeasurementPoint mp) {
+		return new GeoJsonPropertiesDto(2022); //TODO, where to get year from?
+	}
+}
