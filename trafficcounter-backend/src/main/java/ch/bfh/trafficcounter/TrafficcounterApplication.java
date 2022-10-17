1536be9133084dea1304966731ab2b0bf6cede91
Sven Trachsel <sven.projects@protonmail.ch>
2022-10-17 10:33:42 +0200

    cleanup

--- trafficcounter-backend/src/main/java/ch/bfh/trafficcounter/TrafficcounterApplication.java
+++ trafficcounter-backend/src/main/java/ch/bfh/trafficcounter/TrafficcounterApplication.java
@@ -1,10 +1,5 @@
 package ch.bfh.trafficcounter;
 
-import ch.bfh.trafficcounter.service.api.OpenTransportDataApiService;
-import ch.bfh.trafficcounter.service.api.OpenTransportDataApiServiceImpl;
-import ch.opentdata.wsdl.D2LogicalModel;
-import ch.opentdata.wsdl.PullService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,20 +8,8 @@ import javax.annotation.PostConstruct;
 @SpringBootApplication
 public class TrafficcounterApplication {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(TrafficcounterApplication.class, args);
 	}
-
-	@Autowired
-	private OpenTransportDataApiService api;
-
-	/*
-	@PostConstruct
-	public void init() {
-		D2LogicalModel response = api.pullMeasuredData();
-		System.out.println();
-	}
-	 */
 
 }
