89ce8e8f6635c4588b5bbcbb71f81745a2f2f6bc
Sven Trachsel <sven.projects@protonmail.ch>
2022-10-17 18:01:58 +0200

    made Entity more beautiful using lombok

--- trafficcounter-backend/src/main/java/ch/bfh/trafficcounter/model/entity/MeasurementPoint.java
+++ trafficcounter-backend/src/main/java/ch/bfh/trafficcounter/model/entity/MeasurementPoint.java
@@ -24,43 +24,4 @@ public class MeasurementPoint {
 
 	private boolean active;
 
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public double getLatitude() {
-		return latitude;
-	}
-
-	public void setLatitude(double latitude) {
-		this.latitude = latitude;
-	}
-
-	public double getLongtitude() {
-		return longtitude;
-	}
-
-	public void setLongtitude(double longtitude) {
-		this.longtitude = longtitude;
-	}
-
-	public int getNumberOfLanes() {
-		return numberOfLanes;
-	}
-
-	public void setNumberOfLanes(int numberOfLanes) {
-		this.numberOfLanes = numberOfLanes;
-	}
-
-	public boolean isActive() {
-		return active;
-	}
-
-	public void setActive(boolean active) {
-		this.active = active;
-	}
 }
