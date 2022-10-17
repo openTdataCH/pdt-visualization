f18801eab5fe09d87e98cb3f663de0c4b689f901
Sven Trachsel <sven.projects@protonmail.ch>
2022-10-17 10:02:48 +0200

    added usage of dtomapper getAllMeasurementPointsGeoJson

--- trafficcounter-backend/src/main/java/ch/bfh/trafficcounter/service/MeasurementPointService.java
+++ trafficcounter-backend/src/main/java/ch/bfh/trafficcounter/service/MeasurementPointService.java
@@ -1,12 +1,15 @@
 package ch.bfh.trafficcounter.service;
 
+import ch.bfh.trafficcounter.mapper.DtoMapper;
 import ch.bfh.trafficcounter.model.dto.geojson.GeoJsonFeatureCollectionDto;
+import ch.bfh.trafficcounter.model.entity.MeasurementPoint;
+import ch.bfh.trafficcounter.repository.MeasurementPointRepository;
 import ch.bfh.trafficcounter.service.api.OpenTransportDataApiService;
-import ch.opentdata.wsdl.D2LogicalModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
 
 @Service
 public class MeasurementPointService {
@@ -14,17 +17,35 @@ public class MeasurementPointService {
 	@Autowired
 	private OpenTransportDataApiService api;
 
+	private final MeasurementPointRepository measurementPointRepository;
+	private final DtoMapper dtoMapper;
+
+	@Autowired
+	public MeasurementPointService(MeasurementPointRepository measurementPointRepository, DtoMapper dtoMapper){
+		this.measurementPointRepository = measurementPointRepository;
+		this.dtoMapper = dtoMapper;
+	}
+
 
 	public GeoJsonFeatureCollectionDto getAllMeasurementPointsGeoJson() {
-		//TODO, get data from repositories and put into geojsondtos
-		return null;
+
+		ArrayList<MeasurementPoint> measurementPoints = new ArrayList<>(); //TODO, get data from repository
+		return dtoMapper.mapMeasurementPointsToGeoJsonFeatureCollectionDto(measurementPoints);
 	}
 
 	@PostConstruct
 	public void init() {
+		//ArrayList<MeasurementPoint> measurementPoints;
+		//D2LogicalModel staticData = api.pullMeasurementSiteTable();
+
+		//staticData.getPayloadPublication();
+
+		//measurementPointRepository.saveAll(measurementPoints);
+
+
 		//TODO, request data and save into database
 		//D2LogicalModel response = api.pullMeasurementSiteTable();
-		//System.out.println();
+		System.out.println();
 		//Meas
 	}
 
