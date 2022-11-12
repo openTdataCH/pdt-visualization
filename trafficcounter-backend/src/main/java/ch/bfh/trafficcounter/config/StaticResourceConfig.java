aead242dc92ff2332a390beeac8a4a4168c8c15d
Manuel Riesen <manuel.riesen0@gmail.com>
2022-11-13 00:40:26 +0100

    adjust build configuration

---
+++ trafficcounter-backend/src/main/java/ch/bfh/trafficcounter/config/StaticResourceConfig.java
@@ -0,0 +1,16 @@
+package ch.bfh.trafficcounter.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class StaticResourceConfig implements WebMvcConfigurer {
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = { "classpath:/trafficcounter-frontend/" };
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+            .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }
+}
