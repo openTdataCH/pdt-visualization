927e7476238883fa68a3ffd27d342fb3eb35bfc9
Sven Trachsel <sven.projects@protonmail.ch>
2022-11-21 21:48:10 +0100

    VehicleData Endpoint for combined requests, not yet working

---
+++ trafficcounter-backend/src/main/java/ch/bfh/trafficcounter/controller/VehicleDataController.java
@@ -0,0 +1,55 @@
+package ch.bfh.trafficcounter.controller;
+
+import ch.bfh.trafficcounter.event.UpdateEvent;
+import ch.bfh.trafficcounter.model.dto.geojson.GeoJsonFeatureCollectionDto;
+import ch.bfh.trafficcounter.service.VehicleAmountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Sinks;
+
+/**
+ * Controller for combined vehicle data
+ *
+ * @author Sven Trachsel
+ */
+@RestController
+@RequestMapping("/api/vehicledata")
+public class VehicleDataController {
+
+	private final VehicleAmountService vehicleAmountService;
+
+	private final Sinks.Many<UpdateEvent> updateEvent;
+
+	@Autowired
+	public VehicleDataController(VehicleAmountService vehicleAmountService, Sinks.Many<UpdateEvent> updateEvent) {
+		this.vehicleAmountService = vehicleAmountService;
+		this.updateEvent = updateEvent;
+	}
+
+	/**
+	 * Gets the current amount of vehicles and their speed as GeoJson.
+	 *
+	 * @return GeoJson including amount of vehicles
+	 */
+	@GetMapping
+	public ResponseEntity<GeoJsonFeatureCollectionDto> getCurrentVehicleData() {
+		return ResponseEntity.ok(vehicleAmountService.getCurrentVehicleAmount());
+	}
+
+	/**
+	 * Gets the current amount of vehicles and their speed as GeoJson in a reactive way.
+	 * The speed data can be consumed by SSE.
+	 *
+	 * @return GeoJson including amount of vehicles
+	 */
+	@GetMapping(path = "/stream-flux", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public Flux<GeoJsonFeatureCollectionDto> getCurrentVehicleDataReactive() {
+		return updateEvent.asFlux().map(event -> vehicleAmountService.getCurrentVehicleAmount());
+	}
+
+}
