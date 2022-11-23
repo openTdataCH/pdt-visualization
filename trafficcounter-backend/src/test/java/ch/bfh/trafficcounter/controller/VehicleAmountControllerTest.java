9cc30bab97f4a5454585555f0858295678442e9c
Sven Trachsel <sven.projects@protonmail.ch>
2022-11-23 17:40:22 +0100

    remove problematic line in test for testing

--- trafficcounter-backend/src/test/java/ch/bfh/trafficcounter/controller/VehicleAmountControllerTest.java
+++ trafficcounter-backend/src/test/java/ch/bfh/trafficcounter/controller/VehicleAmountControllerTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.test.annotation.DirtiesContext;
 
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
 
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class VehicleAmountControllerTest extends AbstractApiTest {
