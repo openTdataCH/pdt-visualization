292d33a7e618b48c17a2df38caa738e33ea3e8be
Sven Trachsel <sven.projects@protonmail.ch>
2022-12-02 10:40:46 +0100

    delete unused endpoints with tests

--- trafficcounter-backend/src/test/java/ch/bfh/trafficcounter/controller/VehicleDataControllerTest.java
+++ trafficcounter-backend/src/test/java/ch/bfh/trafficcounter/controller/VehicleDataControllerTest.java
@@ -4,6 +4,9 @@ import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.annotation.DirtiesContext;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class VehicleDataControllerTest extends AbstractApiTest {
 
@@ -13,6 +16,10 @@ public class VehicleDataControllerTest extends AbstractApiTest {
 				.get("/api/vehicledata")
 				.then()
 				.assertThat()
+				.body("features.size()", is(1))
+				.body("features[0].properties.speedData.averageSpeed", equalTo(10f))
+				.body("features[0].properties.speedData.speedDisplayClass", equalTo("high"))
+				.body("features[0].properties.vehicleAmount.numberOfVehicles", equalTo(1))
 				.statusCode(HttpStatus.SC_OK);
 	}
 
