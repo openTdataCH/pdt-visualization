0d61d6f5afb53e56c3e88a02eb32a4851907fd81
Manuel Riesen <manuel.riesen0@gmail.com>
2022-12-02 14:11:14 +0100

    cleanup, remove unused services

--- trafficcounter-frontend/src/app/pages/map/components/info-card/info-card.component.spec.ts
+++ trafficcounter-frontend/src/app/pages/map/components/info-card/info-card.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InfoCardComponent } from './info-card.component';
+import {MaterialModule} from "../../../../material/material.module";
+import {TranslateModule} from "@ngx-translate/core";
 
 describe('InfoCardComponent', () => {
   let component: InfoCardComponent;
@@ -8,12 +10,26 @@ describe('InfoCardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ InfoCardComponent ]
+      declarations: [ InfoCardComponent ],
+      imports: [
+        MaterialModule,
+        TranslateModule.forRoot()
+      ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(InfoCardComponent);
     component = fixture.componentInstance;
+    component.info = {
+      id: 1,
+      speedData: {
+        averageSpeed: 100,
+        speedDisplayClass: 'high'
+      },
+      vehicleAmount: {
+        numberOfVehicles: 1
+      }
+    };
     fixture.detectChanges();
   });
 
