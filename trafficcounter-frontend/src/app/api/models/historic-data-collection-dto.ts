20e0f30e79b46cd38b0a2d6445b68e13ea369c02
Manuel Riesen <manuel.riesen0@gmail.com>
2022-12-19 17:32:52 +0100

    add measurement stats type to frontend

--- trafficcounter-frontend/src/app/api/models/historic-data-collection-dto.ts
+++ trafficcounter-frontend/src/app/api/models/historic-data-collection-dto.ts
@@ -1,6 +1,7 @@
 import { HistoricDataDto } from './historic-data-dto';
+import {MeasurementStatsType} from "./measurement-stats-type";
 
 export interface HistoricDataCollectionDto {
-    resolution: string;
+    resolution: MeasurementStatsType;
     measurements: Array<HistoricDataDto>;
 }
